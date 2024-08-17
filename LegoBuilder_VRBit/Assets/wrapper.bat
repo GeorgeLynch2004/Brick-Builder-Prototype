8ebc42b8c217e77ebc1dff6ad34bc8f1f9c385c0
c22037018 <LynchG8@cardiff.ac.uk>
2024-08-17 17:25:02 +0100

    dual_screen_implementation

---
+++ LegoBuilder_VRBit/Assets/wrapper.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "Z:\github\VRbit-Brick-Builder-Prototype\LegoBuilder_VRBit\Builds\v1\LegoBuilder_VRBit.exe"
+python "Z:\github\VRbit-Brick-Builder-Prototype\LegoBuilder_VRBit\Assets\Hand Tracking\main.py"
