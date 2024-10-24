c9f23f7c969671d6f0b936936c1ac3d482323736
c22037018 <LynchG8@cardiff.ac.uk>
2024-10-24 23:30:45 +0100

    commit

--- LegoBuilder_VRBit/Assets/wrapper.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" "Z:\github\VRbit-Brick-Builder-Prototype\LegoBuilder_VRBit\Builds\v1\LegoBuilder_VRBit.exe"
-python "Z:\github\VRbit-Brick-Builder-Prototype\LegoBuilder_VRBit\Assets\Hand Tracking\main.py"
